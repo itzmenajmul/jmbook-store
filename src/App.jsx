974ddc53048d58edd2743b1373763f3642871150
najmul-islam <itzmenajmul@gmail.com>
2023-09-13 21:59:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-purple-400">
+        Start project
+      </h1>
+    </div>
+  );
+}
+
+export default App;
